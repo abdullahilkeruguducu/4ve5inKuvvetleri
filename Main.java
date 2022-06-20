a49f27c497f25ac65a07fdedcd1056020534ce62
Abdullah İlker Üğüdücü <105641416+abdullahilkeruguducu@users.noreply.github.com>
2022-06-20 14:50:45 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int k;
+
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("Sinir Sayisi Giriniz :");
+        k = girdi.nextInt();
+
+        for (int i =1; i<=k; i*=4){
+            System.out.println("4'un kuvveti :" + i);
+        }
+        for (int s=1;s<=k;s*=5){
+            System.out.println("5'in kuvveti :" + s);
+        }
+    }
+}
